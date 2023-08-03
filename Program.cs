add841cdf4957be1b74856a158648bacb94f4813
olivia-bigelow <divinthezabrak@gmail.com>
2023-08-03 12:17:05 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+namespace text_formatter
+{
+    internal class Program
+    {
+        /// <summary>
+        /// this program removes new lines and gets rid of headers from the text inputted
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            Console.WriteLine("paste text here, type end when finished:");
+            string temp = Console.ReadLine();
+            while (temp != "end")
+            {
+                sb.Append(temp.Replace("\n", " "));
+                temp = Console.ReadLine();
+            }
+            
+            Console.WriteLine(sb.ToString());
+     
+
+        }
+    }
+}
